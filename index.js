cf797246034912f143cd7ba91bea1e9f02e7a79a
Dinesh Kudva <dineshkudva12@gmail.com>
2021-11-20 17:29:51 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const authRoutes = require("./routes/authRoutes");
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const { requireAuth, checkUser } = require("./middleware/authMiddleware");
+const {
+  requireAuth,
+  checkUser
+} = require("./middleware/authMiddleware");
 const Task = require("./models/Task");
 
 const port = process.env.PORT || 3000;
@@ -14,7 +17,9 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 //set view engine:
 app.set("view engine", "ejs");
@@ -32,15 +37,24 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("*", checkUser);
-app.get("/", (req, res) => res.render("home", { title: "Code Dojo" }));
+app.get("/", (req, res) => res.render("home", {
+  title: "Code Dojo"
+}));
 app.get("/courses", requireAuth, (req, res) =>
-  res.render("courses", { title: "Courses" })
+  res.render("courses", {
+    title: "Courses"
+  })
 );
 app.get("/usertasks", (req, res) => {
   Task.find()
-    .sort({ createdAt: -1 })
+    .sort({
+      createdAt: -1
+    })
     .then((result) => {
-      res.render("usertasks", { title: "Tasks", tasks: result });
+      res.render("usertasks", {
+        title: "Tasks",
+        tasks: result
+      });
     })
     .catch((err) => {
       console.log(err);
